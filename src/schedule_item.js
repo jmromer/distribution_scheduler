c93a59e4e5738ce2ba59b9069ea8fd4e5c9730b9
Jake Romer <jake@jmromer.org>
2022-06-17 23:12:48 -0400

    Add tests, refactoring

--- src/schedule_item.js
+++ src/schedule_item.js
@@ -9,60 +9,78 @@ const { DateTime } = require("luxon");
 const toString = (date) => date.toFormat('yyyy-MM-dd');
 const fromString = (string) => DateTime.fromFormat(string, 'yyyy-MM-dd').setZone("America/New_York");
 
+function scheduledRegistrationDates({ fields }, today = null) {
+  const { date, registration_period, repeat } = fields;
+  const registrationDates = [];
+
+  let currOccurrence = 0;
+  let currDistributionDate = fromString(date);
+  let currRegistrationDate = currDistributionDate.minus({ days: registration_period });
+
+  do {
+    registrationDates.push(toString(currRegistrationDate));
+
+    if (repeat !== "never") {
+      currOccurrence += 1;
+      currDistributionDate = currDistributionDate.plus(recurrenceInterval(fields))
+      currRegistrationDate = currDistributionDate.minus({ days: registration_period });
+    }
+  } while (shouldRepeat(currOccurrence, currDistributionDate, currRegistrationDate, today, fields));
+
+  return registrationDates;
+}
+
+function shouldRepeat(occurrences, distributionDate, registrationDate, today, fields) {
+  const { repeat, repeat_end, repeat_end_after, repeat_end_on_date } = fields;
+
+  if (repeat === "never") { return false; }
+
+  switch (repeat_end) {
+    case "never":
+      return today && (toString(registrationDate) <= toString(fromString(today).plus({ days: fields.registration_period })));
+    case "after":
+      return occurrences < repeat_end_after;
+    case "on_date":
+      return toString(distributionDate) <= repeat_end_on_date;
+  }
+}
+
+function recurrenceInterval({ repeat, repeat_monthly }) {
+  switch (repeat) {
+    case 'daily':
+      return { days: 1 };
+    case 'weekly':
+      return { days: 7 };
+    case 'biweekly':
+      return { days: 14 };
+    case 'monthly':
+      return monthlyRecurrence(repeat_monthly);
+  }
+}
+
+// TODO
+// Handle 'second monday', 'last day', etc
+function monthlyRecurrence(repeat_monthly) {
+  if (!repeat_monthly) { return { months: 1 }; }
+
+  const weekdays = { sunday: 0, monday: 1, tuesday: 2, wednesday: 3, thursday: 4, friday: 5, saturday: 6 };
+  const occurrence = { first: 1, second: 2, third: 3, fourth: 4, last: -1 };
+  const [interval, day] = repeat_monthly.split(' ');
+
+  return [occurrence[interval], weekdays[day]];
+}
+
 // Is today a day on which a distribution event should be created for the
 // item (a schedule record) being processed?
 //
 // Return true if the schedule has a distribution event scheduled
 // `registration_period` days from today, where `registration_period` is a
 // positive integer.
-function hasUpcomingDistribution({ fields }, today) {
-  const { date, registration_period, repeat } = fields;
-  const { repeat_end, repeat_end_after } = fields;
-
-  let currOccurrence = 0;
-  let currDistributionDate = fromString(date);
-  let currRegistrationDate = currDistributionDate.minus({ days: registration_period });
-
-  while (recurrenceIsDesired(currOccurrence, currDistributionDate, today, fields)) {
-    if (toString(currRegistrationDate) === today) { return true }
-
-    currOccurrence += 1;
-    currDistributionDate = currDistributionDate.plus(recurrenceInterval(fields))
-    currRegistrationDate = currRegistrationDate.minus({ days: registration_period });
+function hasUpcomingDistribution(input, today) {
+  for (let registrationDate of scheduledRegistrationDates(input, today)) {
+    if (registrationDate === today) { return true; }
   }
-
-  return false
-}
-
-function recurrenceIsDesired(occurrences, distributionDate, today, { repeat, repeat_end, repeat_end_after, repeat_end_on_date }) {
-  if (repeat === "never") { return false }
-
-  switch (repeat_end) {
-    case "never": return toString(distributionDate) <= today
-    case "after": return occurrences < repeat_end_after
-    case "on_date": return toString(distributionDate) <= repeat_end_on_date
-  }
-}
-
-function recurrenceInterval({ repeat, repeat_monthly }) {
-  switch (repeat) {
-    case 'daily': return { days: 1 }
-    case 'weekly': return { days: 7 }
-    case 'biweekly': return { days: 14 }
-    case 'monthly': return monthlyRecurrence(repeat_monthly)
-  }
-}
-
-// TODO
-// Handle 'second monday', 'last day', etc
-function monthlyRecurrence({ repeat_monthly }) {
-  if (!repeat_monthly) { return { months: 1 } }
-
-  const weekdays = { sunday: 0, monday: 1, tuesday: 2, wednesday: 3, thursday: 4, friday: 5, saturday: 6 }
-  const occurrence = { first: 1, second: 2, third: 3, fourth: 4, last: -1 }
-  const [interval, day] = repeat_monthly.split(' ')
-
-  return [occurrence[interval], weekdays[day]]
+  return false;
 }
 
 function run(item) {
@@ -72,9 +90,5 @@ function run(item) {
 }
 
 module.exports = {
-  run,
-  hasUpcomingDistribution,
-  recurrenceIsDesired,
-  recurrenceInterval,
-  monthlyRecurrence,
+  scheduledRegistrationDates,
 }
--- src/schedule_item.test.js
+++ src/schedule_item.test.js
@@ -1,39 +1,104 @@
-// Docs:
-// https://docs.n8n.io/nodes/n8n-nodes-base.functionItem
-// https://github.com/moment/luxon
-// https://jmespath.org
-
 const tk = require("timekeeper");
 const { DateTime } = require("luxon");
-const node = require("./schedule_item");
 
-const freezeDate = (dateString) => {
-  const date = new Date(`${dateString}T00:00:00.000-04:00`).toISOString().split('T')[0];
-  tk.freeze(date);
-  return date;
-}
+const node = require("./schedule_item");
 
 afterEach(() => tk.reset());
 
-test("returns true if registration opens today for the event being scheduled", () => {
-  const today = freezeDate('2022-06-14');
-  const item = {
+// #scheduledRegistrationDates
+it("not repeating: returns a single registration date", () => {
+  let item = buildSchedule({ date: '2022-06-15', registration_period: 3, repeat: "never" });
+  let registrationDates = node.scheduledRegistrationDates(item);
+  expect(registrationDates).toEqual(["2022-06-12"]);
+});
+
+it("repeating daily: returns a list of dates on which registration opens", () => {
+  // ending after n times
+  let item = buildSchedule({ date: '2022-06-15', registration_period: 5, repeat: "daily", repeat_end: "after", repeat_end_after: 10 });
+  let registrationDates = node.scheduledRegistrationDates(item);
+  expect(registrationDates).toEqual([
+    "2022-06-10", "2022-06-11", "2022-06-12", "2022-06-13", "2022-06-14",
+    "2022-06-15", "2022-06-16", "2022-06-17", "2022-06-18", "2022-06-19",
+  ]);
+
+  item = buildSchedule({ date: '2022-06-15', registration_period: 5, repeat: "daily", repeat_end: "after", repeat_end_after: 5 });
+  registrationDates = node.scheduledRegistrationDates(item);
+  expect(registrationDates).toEqual(["2022-06-10", "2022-06-11", "2022-06-12", "2022-06-13", "2022-06-14"]);
+
+  // ending on a date
+  item = buildSchedule({ date: '2022-06-15', registration_period: 5, repeat: "daily", repeat_end: "on_date", repeat_end_on_date: "2022-06-17", });
+  registrationDates = node.scheduledRegistrationDates(item);
+  expect(registrationDates).toEqual(["2022-06-10", "2022-06-11", "2022-06-12"]);
+
+  // no defined ending
+  let today = freezeDate('2022-06-20');
+  item = buildSchedule({ date: '2022-06-15', registration_period: 5, repeat: "daily", repeat_end: "never", });
+  registrationDates = node.scheduledRegistrationDates(item, today);
+  expect(registrationDates.length).toEqual(16);
+  expect(registrationDates[0]).toEqual("2022-06-10");
+  expect(registrationDates[15]).toEqual("2022-06-25");
+});
+
+it("repeating weekly: returns a list of dates on which registration opens", () => {
+  let item = buildSchedule({ date: '2022-06-08', registration_period: 7, repeat: "weekly", repeat_end: "after", repeat_end_after: 3 });
+  let registrationDates = node.scheduledRegistrationDates(item);
+  expect(registrationDates).toEqual(["2022-06-01", "2022-06-08", "2022-06-15"]);
+
+  item = buildSchedule({ date: '2022-06-08', registration_period: 7, repeat: "weekly", repeat_end: "after", repeat_end_after: 1 });
+  registrationDates = node.scheduledRegistrationDates(item);
+  expect(registrationDates).toEqual(["2022-06-01"]);
+});
+
+it("repeating biweekly: returns a list of dates on which registration opens", () => {
+  let item = buildSchedule({ date: '2022-06-08', registration_period: 7, repeat: "biweekly", repeat_end: "after", repeat_end_after: 3 });
+  let registrationDates = node.scheduledRegistrationDates(item);
+  expect(registrationDates).toEqual(["2022-06-01", "2022-06-15", "2022-06-29"]);
+
+  item = buildSchedule({ date: '2022-06-08', registration_period: 7, repeat: "biweekly", repeat_end: "after", repeat_end_after: 1 });
+  registrationDates = node.scheduledRegistrationDates(item);
+  expect(registrationDates).toEqual(["2022-06-01"]);
+});
+
+it("repeating monthly: returns a list of dates on which registration opens", () => {
+  let item = buildSchedule({ date: '2022-06-08', registration_period: 7, repeat: "monthly", repeat_end: "after", repeat_end_after: 3 });
+  let registrationDates = node.scheduledRegistrationDates(item);
+  expect(registrationDates).toEqual(["2022-06-01", "2022-07-01", "2022-08-01"]);
+
+  item = buildSchedule({ date: '2022-06-08', registration_period: 7, repeat: "monthly", repeat_end: "after", repeat_end_after: 1 });
+  registrationDates = node.scheduledRegistrationDates(item);
+  expect(registrationDates).toEqual(["2022-06-01"]);
+});
+
+// it("repeating monthly (nth weekday): returns a list of dates on which registration opens", () => {
+// });
+
+// it("repeating monthly (nth day): returns a list of dates on which registration opens", () => {
+// });
+
+const freezeDate = (dateString) => {
+  tk.freeze(dateString);
+  return dateString;
+}
+
+const buildSchedule = fields => {
+  return {
     id: "recklvKSxYpBO1uic",
     createdTime: "2022-06-17T12:41:08.000Z",
     fields: {
       "id": 18,
       "program_record_id": ["recqTIajb3BBJR8Bl"],
-      "date": "2022-06-17",
-      "repeat": "daily",
+      "date": fields.date,
+      "registration_period": fields.registration_period,
+      "repeat": fields.repeat,
+      "repeat_end": fields.repeat_end,
+      "repeat_end_after": fields.repeat_end_after,
+      "repeat_end_on_date": fields.repeat_end_on_date,
+      "repeat_monthly": fields.repeat_monthly,
       "start_time": "10:00",
       "end_time": "13:00",
-      "repeat_end": "after",
-      "repeat_end_after": 3,
-      "registration_period": 3,
       "program_id": ["recqTIajb3BBJR8Bl"],
       "org_name": ["Help Kitchen"],
       "program_name": ["Bill's Program"]
     }
   };
-  expect(node.hasUpcomingDistribution(item, today)).toEqual(true);
-});
+};
